38a348c63bc3e90f4395f6423928d5602632dbed
BODIK <mka@A34PC2>
2025-02-25 12:43:15 +0200

    bkhjhj

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("--------- Welcome ---------");
+
+Console.WriteLine("--------- Menu ---------");
+Console.WriteLine("--------- 1. Poslugy ---------");
+Console.WriteLine("--------- 2. Redaguvaty poslugy ---------");
+Console.WriteLine("--------- 3. Priyom zamovlen' ---------");
+Console.WriteLine("--------- 4. Vykonannia zamovlenna ---------");
+Console.WriteLine("--------- 5. Zberezhennya zamovlen ---------");
+
+
+
+
