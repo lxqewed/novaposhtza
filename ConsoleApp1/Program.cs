18156e335bdfcd8ec35f753a3e850f65cc8a2a5d
mka <mka@A34PC1>
2025-04-08 14:43:56 +0300

    іуав немоьго лбуцва 76ни9

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.OutputEncoding = System.Text.Encoding.UTF8; // для укр. тексту
 Console.WriteLine("--------- Welcome ---------");
 
-Console.WriteLine("--------- Menu ---------");
-Console.WriteLine("--------- 1. Poslugy ---------");
-Console.WriteLine("--------- 2. Redaguvaty poslugy ---------");
-Console.WriteLine("--------- 3. Priyom zamovlen' ---------");
-Console.WriteLine("--------- 4. Vykonannia zamovlenna ---------");
-Console.WriteLine("--------- 5. Zberezhennya zamovlen ---------");
+Console.WriteLine("--------- Меню ---------");
+Console.WriteLine("--------- 1. Послуги ---------");
+Console.WriteLine("--------- 2. Редагувати послуги ---------");
+Console.WriteLine("--------- 3. Прийом замовлень ---------");
+Console.WriteLine("--------- 4. Виконання замовлення ---------");
+Console.WriteLine("--------- 5. Збереження замовлень ---------");
 
+public class Посилка
+{
+    public string Назва { get; set; }
+    public string Адреса { get; set; }
+    public string Отримувач { get; set; }
+    public string Відправник { get; set; }
+    public double Вага { get; set; }
+    public double Ціна { get; set; }
 
 
 
